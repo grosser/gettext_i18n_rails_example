6ae715d46fa91d51325e6af30bdf0a98a9523058
grosser <grosser.michael@gmail.com>
2009-02-28 11:34:51 +0200

    plugin updates, completely translated app

--- config/environment.rb
+++ config/environment.rb
@@ -18,8 +18,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   config.gem "sqlite3-ruby", :lib => "sqlite3"
-  config.gem "grosser-fast_gettext", :lib => 'fast_gettext', :version => '0.2.10', :source=>"http://gems.github.com/"
-#  config.gem "fast_gettext", :version => '0.2.10'
+  config.gem "grosser-fast_gettext", :lib => 'fast_gettext', :version => '~>0.2.10', :source=>"http://gems.github.com/"
   # config.gem "aws-s3", :lib => "aws/s3"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
