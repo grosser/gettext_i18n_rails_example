4a6aa914cc8e4056bcf2a934e8c5f2bed5a4e9ae
grosser <grosser.michael@gmail.com>
2009-05-10 17:16:54 +0200

    updated plugins

---
+++ vendor/plugins/gettext_i18n_rails/lib/gettext_i18n_rails/backend.rb
@@ -0,0 +1,35 @@
+module GettextI18nRails
+  #translates i18n calls to gettext calls
+  class Backend
+    attr_accessor :backend
+
+    def initialize(*args)
+      self.backend = I18n::Backend::Simple.new(*args)
+    end
+
+    def available_locales
+      FastGettext.available_locales || []
+    end
+
+    def translate(locale, key, options)
+      flat_key = flatten_key key, options
+      if FastGettext.key_exist?(flat_key)
+        raise "no yet build..." if options[:locale]
+        _(flat_key)
+      else
+        backend.translate locale, key, options
+      end
+    end
+
+    def method_missing(method, *args)
+      backend.send(method, *args)
+    end
+
+    protected
+
+    def flatten_key key, options
+      scope = [*(options[:scope] || [])]
+      scope.empty? ? key.to_s : "#{scope*'.'}.#{key}"
+    end
+  end
+end
