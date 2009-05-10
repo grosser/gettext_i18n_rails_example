8b9847386a75d7da8416b63fdf35afd0c1a16bf7
grosser <grosser.michael@gmail.com>
2009-05-10 17:43:09 +0200

    added a model validation translation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,5 @@
 class Car < ActiveRecord::Base
+  N_('activerecord.errors.models.car.attributes.model.blank') #to mark that we need this translation
   validates_presence_of :model
   validates_numericality_of :wheels_count, :only_integer=>true, :greater_than=>3,:less_than=>9
 end
