c4932fe9a7a08ad4e00d359d2e17355dde6a8dd8
grosser <grosser.michael@gmail.com>
2011-02-13 09:07:07 +0100

    remove duplication in translation

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,5 @@ class Car < ActiveRecord::Base
   N_('activerecord.errors.models.car.attributes.model.blank') #to mark that we need this translation
   validates_presence_of :model
   validates_numericality_of :wheels_count, :only_integer=>true, :greater_than=>3,:less_than=>9
-  validates_presence_of :country, :message => N_('country should not be blank')
+  validates_presence_of :country, :message => N_('country|should not be blank')
 end
